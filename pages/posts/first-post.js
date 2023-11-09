5b74684474027a8aeb1b7c2024d4663ff7c851cf
ins1d30ut <nfteebsv@protonmail.com>
2023-11-09 13:14:51 -0500

    commit layout and _app.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Script from 'next/script';
 import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         <Script
@@ -17,8 +18,8 @@ export default function FirstPost() {
       </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href='/'>← Back to home</Link>
+        <Link href='/'>Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
